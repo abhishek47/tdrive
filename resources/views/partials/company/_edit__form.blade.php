33d4ff0de6d814305cf203d46f327f027a10b811
Abhishek Wani <Trumpets@Abhisheks-iMac.local>
2017-08-15 12:08:14 +0530

    Form Inputs Design

--- resources/views/partials/company/_edit__form.blade.php
+++ resources/views/partials/company/_edit__form.blade.php
@@ -2,21 +2,21 @@
     <div class="col-md-12">
         <form class="form-horizontal" role="form">
             <div class="form-group">
-                <label for="inputEmail1" class="">Company Name</label>
+                <label for="inputEmail1" class="control-label">Company Name</label>
                   <input class="form-control" id="inputEmail1" placeholder="Email" type="email">
                     <p class="help-block">Example block-level help text here.</p>
                 
             </div>
 
             <div class="form-group">
-                <label for="inputEmail1" class="">Company Email</label>
+                <label for="inputEmail1" class="control-label">Company Email</label>
                   <input class="form-control" id="inputEmail1" placeholder="Email" type="email">
                     <p class="help-block">Example block-level help text here.</p>
                 
             </div>
 
             <div class="form-group">
-                <label for="inputEmail1" class="">Address</label>
+                <label for="inputEmail1" class="control-label">Address</label>
                   <input class="form-control" id="inputEmail1" placeholder="Email" type="email">
                     <p class="help-block">Example block-level help text here.</p>
              
@@ -24,42 +24,42 @@
 
 
             <div class="form-group">
-                <label for="inputEmail1" class="">Conutry</label>
+                <label for="inputEmail1" class="control-label">Conutry</label>
                   <input class="form-control" id="inputEmail1" placeholder="Email" type="email">
                     <p class="help-block">Example block-level help text here.</p>
                 
             </div>
 
              <div class="form-group">
-                <label for="inputEmail1" class="">City</label>
+                <label for="inputEmail1" class="control-label">City</label>
                   <input class="form-control" id="inputEmail1" placeholder="Email" type="email">
                     <p class="help-block">Example block-level help text here.</p>
                 
             </div>
 
              <div class="form-group">
-                <label for="inputEmail1" class="">State</label>
+                <label for="inputEmail1" class="control-label">State</label>
                   <input class="form-control" id="inputEmail1" placeholder="Email" type="email">
                     <p class="help-block">Example block-level help text here.</p>
                 
             </div>
 
              <div class="form-group">
-                <label for="inputEmail1" class="">Zip</label>
+                <label for="inputEmail1" class="control-label">Zip</label>
                   <input class="form-control" id="inputEmail1" placeholder="Email" type="email">
                     <p class="help-block">Example block-level help text here.</p>
                 
             </div>
 
              <div class="form-group">
-                <label for="inputEmail1" class="">Phone</label>
+                <label for="inputEmail1" class="control-label">Phone</label>
                   <input class="form-control" id="inputEmail1" placeholder="Email" type="email">
                     <p class="help-block">Example block-level help text here.</p>
                 
             </div>
 
              <div class="form-group">
-                <label for="inputEmail1" class="">Website</label>
+                <label for="inputEmail1" class="control-label">Website</label>
                   <input class="form-control" id="inputEmail1" placeholder="Email" type="email">
                     <p class="help-block">Example block-level help text here.</p>
                 
@@ -67,7 +67,7 @@
          
             <div class="form-group">
                 <div class="">
-                    <button type="submit" class="btn btn-info">Update Details</button>
+                    <button type="submit" class="btn btn-success">Update Details</button>
                 </div>
 
             </div>
