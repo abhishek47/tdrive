921daebfeb822a5c43d2fc1c735b0a453430229d
abhishek wani <abhishek wani>
2017-09-03 10:52:41 +0530

    Branches

--- app/Http/Controllers/BranchesController.php
+++ app/Http/Controllers/BranchesController.php
@@ -6,6 +6,18 @@ use Illuminate\Http\Request;
 
 class BranchesController extends Controller
 {
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        $this->middleware('auth');
+
+        $this->middleware('company-registered');
+    }
+    
     public function index()
     {
     	$branches = auth()->user()->company->branches;
